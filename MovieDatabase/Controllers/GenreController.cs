f9a4afeb801578d55b57a495fa7f871891ba51c6
Jerrad Monagan <jerradmonagan@gmail.com>
2017-11-07 09:18:40 -0500

    First commit, genre done

---
+++ MovieDatabase/Controllers/GenreController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MovieDatabase.Models;
+
+namespace MovieDatabase.Controllers
+{
+    public class GenreController : Controller
+    {
+        // GET: Genre
+        public ActionResult Index()
+        {
+            GenreDBHandler dbhandle = new GenreDBHandler();
+            ModelState.Clear();
+            return View(dbhandle.GetGenre());
+        }
+
+        // GET: Genre/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Genre/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Genre/Create
+        [HttpPost]
+        public ActionResult Create(Genre smodel)
+        {
+            try 
+            {
+                if (ModelState.IsValid)
+                { 
+                    GenreDBHandler sdb = new GenreDBHandler();
+                    if (sdb.AddGenre(smodel))
+                    {
+                        ViewBag.Message = "Genre Details Added Successfully";
+                     ModelState.Clear();
+                    }
+            }
+            return View();
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Genre/Edit/5
+        public ActionResult Edit(int id)
+        {
+            GenreDBHandler sdb = new GenreDBHandler();
+            return View(sdb.GetGenre().Find(smodel => smodel.GenreID == id));
+        }
+
+        // POST: Genre/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Genre smodel)
+        {
+            try
+            {
+                GenreDBHandler sdb = new GenreDBHandler();
+                sdb.UpdateDetails(smodel);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Genre/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                GenreDBHandler sdb = new GenreDBHandler();
+                if (sdb.DeleteGenre(id))
+                {
+                    ViewBag.AlertMsg = "Genre Deleted Successfully";
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // POST: Genre/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MovieDatabase/Models/GenreDBHandler.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace MovieDatabase.Models
+{
+    public class GenreDBHandler
+    {
+        private SqlConnection con;
+        private void connection()
+        {
+            string constring = ConfigurationManager.ConnectionStrings["MovieConn"].ToString();
+            con = new SqlConnection(constring);
+        }
+
+        // **************** ADD NEW Genre *********************
+
+        public bool AddGenre(Genre smodel)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("AddNewGenre", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@Name", smodel.Name);
+       
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+        // ********** VIEW Genre DETAILS ********************
+        public List<Genre> GetGenre()
+        {
+            connection();
+            List<Genre> Genrelist = new List<Genre>();
+
+            SqlCommand cmd = new SqlCommand("GetGenreDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            sd.Fill(dt);
+            con.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                Genrelist.Add(
+                    new Genre
+                    {
+                        GenreID = Convert.ToInt32(dr["GenreID"]),
+                        Name = Convert.ToString(dr["Name"])
+                    });
+            }
+            return Genrelist;
+        }
+
+        // ***************** UPDATE Genre DETAILS *********************
+        public bool UpdateDetails(Genre smodel)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("UpdateGenreDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@GenreID", smodel.GenreID);
+            cmd.Parameters.AddWithValue("@Name", smodel.Name);
+    
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+
+        // ********************** DELETE Genre DETAILS *******************
+        public bool DeleteGenre(int id)
+        {
+            connection();
+            SqlCommand cmd = new SqlCommand("DeleteGenre", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@GenreID", id);
+
+            con.Open();
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            if (i >= 1)
+                return true;
+            else
+                return false;
+        }
+    }
+}
